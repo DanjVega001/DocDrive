09f78fed3d119ee09c64f8a10f5a02ac06e9a431
Daniel Vega <daniel.vega03@hotmail.com>
2024-05-15 16:24:29 -0500

    Init repo, and models and migrations created

---
+++ app/Models/Validity.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Validity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'startYear',
+        'endYear',
+    ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class, "validity_id");
+    }
+
+}
---
+++ database/migrations/2024_05_15_191835_create_validities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateValiditiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('validities', function (Blueprint $table) {
+            $table->id();
+            $table->integer("startYear");
+            $table->integer("endYear");
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('validities');
+    }
+}
